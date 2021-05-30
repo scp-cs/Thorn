1d279b494b32fe9a717592b022adf62206aa391f
Aidam <adam.pekac@gmail.com>
2021-05-30 20:34:22 +0200

    Added :eye: reaction to .vote (Issue #27)

--- thorn/Modules/AdminModule.cs
+++ thorn/Modules/AdminModule.cs
@@ -152,7 +152,9 @@ namespace thorn.Modules
             await msg.AddReactionAsync(Emote.Parse(_pairs.GetString("YES_EMOTE")));
             await msg.AddReactionAsync(Emote.Parse(_pairs.GetString("NO_EMOTE")));
             await msg.AddReactionAsync(Emote.Parse(_pairs.GetString("ABSTAIN_EMOTE")));
-            await msg.AddReactionAsync(new Emoji("👁️"));
+            // I swear to god, since when are there 2 different eye emojis?
+            //await msg.AddReactionAsync(new Emoji("👁️")); This one doesn't work, I think
+            await msg.AddReactionAsync(new Emoji("👁"));
         }
     }
 }
